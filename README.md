8289ea4f6b85228aad40b317d5427da1d68fd83e
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-03 12:15:58 +1000

    colors figured out. Dependencies listed

--- README.md
+++ README.md
@@ -135,3 +135,10 @@ You are required to submit the following:
 
 - - -
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
+
+
+
+///////////////////////////////////////
+My Stuff
+https://www.npmjs.com/package/colors
---
+++ app.js
@@ -0,0 +1,49 @@
+//Dependencies
+const inquirer = require("inquirer"); 
+const mysql = require("mysql"); 
+const consoleTable = require("console.table");
+const colors = require("colors")
+colors.enable(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Figuring out colors. 
+//For reference only.
+//Delete after app. 
+var x = myFunction(4, 3);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+  return a * b;  
+             // Function returns the product of a and b
+  
+}
+
+myFunction(); 
+console.log('x'.green); 
+console.log(colors.rainbow('Hey'), x); 
+
