77ef1eb7a0c90cafb34305122073e536571cff2d
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-03 12:36:09 +1000

    figured out how to use console.table

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ colors.enable();
 //Figuring out colors. 
 //For reference only.
 //Delete after app. 
+
+/*
 var x = myFunction(4, 3);   // Function is called, return value will end up in x
 
 function myFunction(a, b) {
@@ -46,4 +48,17 @@ function myFunction(a, b) {
 myFunction(); 
 console.log('x'.green); 
 console.log(colors.rainbow('Hey'), x); 
+*/ 
 
+/*
+//Basic stuff for figuring out tables. 
+const table = ["team 1", "team2", "team3", "team4", "team5"];
+const table1 = ["team1", "team2", "1000"];
+const man = {
+    man: "man",
+    age: 1001,
+    hasAJob: "No"
+}
+
+console.table(table, table1, man)
+*/ 
